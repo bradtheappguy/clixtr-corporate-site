3e3223ecf455517a081b94453053efb431c0c723
Brad Smith <bradsmithinc@gmail.com>
2011-04-12 13:16:23 -0700

    Added Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ClixtrCorporateSite::Application.config.secret_token = '3276b875abf34c5802dc682cc15cce3663164e8fc3e5903b26b08a7f1b5ba57bbcc132709381f1738c54c51e25c0574adf20f3c5ed6c842985dad60868aa49c7'
